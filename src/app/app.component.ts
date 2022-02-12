bf00050e69337b3cdaf72cbb7285002d9f6800d5
Georgia Hunt <georgialhunt89@gmail.com>
2022-02-12 10:56:33 +1030

    Onclick function added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password_generator';
+  password ='';
+
+  onButtonClick(){
+    this.password = 'test password';
+    console.log('button was clicked');
+  }
+
+  
 }
