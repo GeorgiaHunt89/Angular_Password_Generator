d5328a05ea2976c8520588536ca2ccbf0d27a99a
Georgia Hunt <georgialhunt89@gmail.com>
2022-02-12 11:08:35 +1030

    Function for checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password ='';
+  includeLetters =false;
+  includeNumbers =false;
+  includeSymbols =false;
+
+  onChangeLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
 
   onButtonClick(){
     this.password = 'test password';
-    console.log('button was clicked');
+    console.log('Include Letters', this.includeLetters);
+    console.log('Include Numbers', this.includeNumbers);
+    console.log('Include Symbols', this.includeSymbols);
   }
 
   
